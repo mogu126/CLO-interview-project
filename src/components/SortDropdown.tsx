09d5f9ffc7701556ec6caa61a3c5d525442662dd
mogu126 <449642939@qq.com>
2025-10-25 20:28:21 +0800

    modified port when running on local and changed select to ul li for sort dropdown

--- src/components/SortDropdown.tsx
+++ src/components/SortDropdown.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { setSortBy } from '../store/slices/contentSlice';
@@ -18,7 +18,11 @@ const SortLabel = styled.label`
   color: #828185;
 `;
 
-const SortSelect = styled.select`
+const DropdownContainer = styled.div`
+  position: relative;
+`;
+
+const DropdownButton = styled.button`
   padding: 13px 12px;
   border: none;
   outline: none;
@@ -26,30 +30,84 @@ const SortSelect = styled.select`
   color: #828185;
   border-bottom: 1px solid #828185;
   background-color: #1B1A21;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 8px;
   
-  option {
-    line-height: 2.5;
-    background-color: #1B1A21;
-    color: #828185;
+  &:after {
+    content: '▼';
+    font-size: 10px;
+  }
+`;
+
+const DropdownList = styled.ul`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  background-color: #1B1A21;
+  border-top: none;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  z-index: 1000;
+`;
+
+const DropdownItem = styled.li`
+  padding: 13px 12px;
+  font-size: 14px;
+  color: #828185;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: #2A2930;
+  }
+  
+  &.selected {
+    background-color: #3A3940;
   }
 `;
 
 const SortDropdown: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { sortBy } = useSelector((state: RootState) => state.content);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    dispatch(setSortBy(e.target.value as SortOption));
+  const options = [
+    { value: 'name', label: 'Item Name' },
+    { value: 'price-high', label: 'Higher Price' },
+    { value: 'price-low', label: 'Lower Price' }
+  ];
+
+  const currentOption = options.find(opt => opt.value === sortBy);
+
+  const handleOptionClick = (value: SortOption) => {
+    dispatch(setSortBy(value));
+    setIsOpen(false);
   };
 
   return (
     <SortContainer>
       <SortLabel>Sort by</SortLabel>
-      <SortSelect value={sortBy} onChange={handleSortChange}>
-        <option value="name">Item Name</option>
-        <option value="price-high">Higher Price</option>
-        <option value="price-low">Lower Price</option>
-      </SortSelect>
+      <DropdownContainer>
+        <DropdownButton onClick={() => setIsOpen(!isOpen)}>
+          {currentOption?.label}
+        </DropdownButton>
+        {isOpen && (
+          <DropdownList>
+            {options.map(option => (
+              <DropdownItem
+                key={option.value}
+                className={option.value === sortBy ? 'selected' : ''}
+                onClick={() => handleOptionClick(option.value as SortOption)}
+              >
+                {option.label}
+              </DropdownItem>
+            ))}
+          </DropdownList>
+        )}
+      </DropdownContainer>
     </SortContainer>
   );
 };
